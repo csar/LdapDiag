2bda7d883af922dbbad78ed7767807c09af798be
csaager <csaager@gmail.com>
2018-02-09 12:51:02 +0100

    Alternate addressing

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -2,6 +2,7 @@ import java.util
 import javax.naming.Context
 import javax.naming.directory.{InitialDirContext, SearchControls}
 
+import com.sun.jndi.ldap.LdapCtx
 import org.xbill.DNS.{ARecord, Lookup, SRVRecord, Type}
 
 import scala.util.Try
@@ -13,16 +14,22 @@ object Run extends App {
   val user = if (args.length > 1) args(1) else "carsten.saager"
   val pass = if (args.length > 2) args(2) else "Fr4nce!!"
   val searchString = if (args.length > 3) args(3) else s"(&(objectClass=user)(sAMAccountName=$user))"
+  val searchBase = domain.split('.').mkString("DC=", ",DC=", "")
 
   import Context._
+
+  var queryRepeat = 1
 
   env.put(INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory")
   env.put(SECURITY_AUTHENTICATION, "simple")
   env.put(SECURITY_PRINCIPAL, s"$user@$domain")
   env.put(SECURITY_CREDENTIALS, pass)
 
-  println(s"In domain '$domain' user '$user' pass '$pass' search '$searchString'")
-  println(checkLdap(domain, 389))
+  env.put(PROVIDER_URL, "ldap:///" + searchBase)
+
+
+
+  queryRepeat = 1
   val lookup = new Lookup("_ldap._tcp." + domain, Type.SRV)
   Try(lookup.run()).map { records =>
     for (record <- records.collect { case s: SRVRecord => s }) {
@@ -39,23 +46,64 @@ object Run extends App {
       println(s"DNS search for SRV records failed with '${lookup.getErrorString}'")
   }
 
+  for (_ <- 1 to 20) {
+    println("Discovery of LDAP:")
+    println(checkLdap(domain, 0))
+    Thread.sleep(500)
+    println(s"In domain '$domain' user '$user' pass '$pass' search '$searchString'")
+    println(checkLdap(domain, 389)) //"alis-dc1-bng.alis.test ldap://alis-dc1-cdf.alis.test ldap://alis-dc1-hl.alis.test"
+  }
   def checkLdap(ip: String, port: Int): Try[Int] = Try {
-    env.put(PROVIDER_URL, s"ldap://$ip:$port")
-    withResource(new InitialDirContext(env)) { context =>
-      val searchControls = new SearchControls
-      import SearchControls._
-      searchControls.setReturningAttributes(Array("sAMAccountName"))
-      searchControls.setSearchScope(SUBTREE_SCOPE)
-      val searchBase = domain.split('.').mkString("DC=", ",DC=", "")
-
-      val values = timed {
-        context.search(searchBase, searchString, searchControls)
-      } { time =>
-        println(s"Query via $ip took ${time}ms")
+    if (port == 0)
+      env.put(PROVIDER_URL, "ldap:///" + searchBase)
+    else
+      env.put(PROVIDER_URL, s"ldap://$ip:$port")
+    val start = System.currentTimeMillis()
+    val ctx = try {
+      new InitialDirContext(env) {
+        def ldap = defaultInitCtx match {
+          case l: LdapCtx =>
+            Try {
+              val hf = l.getClass.getDeclaredField("hostname")
+              hf.setAccessible(true)
+              hf.get(l).toString
+            }.getOrElse("<access failed>")
+          case _ => "<can't read hostname>"
+        }
       }
-
-      withResource(values)(identity(_))
+    } catch {
+      case NonFatal(e) =>
+        print(e)
+        println(start - System.currentTimeMillis())
+        throw e
     }
+    ctx match {
+      case c: {def ldap: String} => print(c.ldap)
+      case _ =>
+    }
+    println(start - System.currentTimeMillis())
+    withResource(ctx) { c =>
+
+      val q = query(ip, if (port == 0) "" else searchBase) _
+      (1 to queryRepeat).foldLeft(0)((_, _) => q(c))
+    }
+
+  }
+
+  def query(ip: String, searchBase: String)(context: InitialDirContext): Int = {
+    val searchControls = new SearchControls
+    import SearchControls._
+    searchControls.setReturningAttributes(Array("sAMAccountName"))
+    searchControls.setSearchScope(SUBTREE_SCOPE)
+
+
+    val values = timed {
+      context.search(searchBase, searchString, searchControls)
+    } { time =>
+      println(s"Query via $ip took ${time}ms")
+    }
+
+    withResource(values)(identity(_))
   }
 
   def timed[T](call: => T)(out: Long => Unit): T = {
